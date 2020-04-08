d8c75054cc8118f4737ca8bfee5a89abd6aa4c5d
derammo <817160+derammo@users.noreply.github.com>
2020-04-08 02:01:19 -0400

    removed test data used during development

--- Tools/TestApplyPatches/test/cases/lines_inserted/a/Mods/aircraft/A-10C/Cockpit/Scripts/UHF_Radio/indicator/freq_status_init.lua
+++
@@ -1,4 +0,0 @@
-dofile(LockOn_Options.script_path.."UHF_Radio/indicator/common_init.lua")
-
-dofile(LockOn_Options.script_path.."UHF_Radio/indicator/indicators.lua")
-indicator = indicators.FREQUENCY_STATUS
--- Tools/TestApplyPatches/test/cases/lines_inserted/b/Mods/aircraft/A-10C/Cockpit/Scripts/UHF_Radio/indicator/repeater_init.lua
+++
@@ -1,9 +0,0 @@
-dofile(LockOn_Options.script_path.."UHF_Radio/indicator/common_init.lua")
-
-purposes = {render_purpose.GENERAL}
-
-dofile(LockOn_Options.script_path.."UHF_Radio/indicator/indicators.lua")
-indicator = indicators.REPEATER
-
-dofile(LockOn_Options.common_script_path.."ViewportHandling.lua")
-try_find_assigned_viewport("A10C_UHFR", "UHF_REPEATER")
