ae22ecc56cb0071155cf4e0d99924b48d27c0c98
derammo <817160+derammo@users.noreply.github.com>
2020-04-15 10:50:29 -0400

    fixed project file bug added message to early exit from build.cmd

--- build.cmd
+++ build.cmd
@@ -5,7 +5,10 @@ MSBuild.exe -binaryLogger:LogFile=clean.binlog -clp:WarningsOnly -warnAsMessage:
 @rmdir /s /q bin
 @MSBuild.exe -binaryLogger:LogFile=prebuild.binlog -clp:WarningsOnly -warnAsMessage:MSB4078 -p:version=%VERSION% -p:Configuration=Release;Platform=x64 Tools\GenerateSimulatorViewportTemplates\GenerateSimulatorViewportTemplates.csproj
 @MSBuild.exe -binaryLogger:LogFile=build.binlog  -clp:WarningsOnly -warnAsMessage:MSB4078 -p:version=%VERSION% -p:Configuration=NoInstallers;Platform=x64
-@if %errorlevel% neq 0 exit /b %errorlevel%
+@if %errorlevel% neq 0 (
+	echo build of "NoInstallers" configuration failed.  Installer will not be built.
+	exit /b %errorlevel%
+)
 
 @REM build installers; this requires https://stackoverflow.com/questions/8648428/an-error-occurred-while-validating-hresult-8000000a/45580775#45580775
 @devenv Helios.sln /Build JustInstallers
