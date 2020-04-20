d392f2b783820d857b88a246cadd4e7b1c184c0a
derammo <817160+derammo@users.noreply.github.com>
2020-04-19 22:57:20 -0400

    implemented status reporting for Profile interface

--- Helios/Interfaces/Profile/ProfileInterface.cs
+++ Helios/Interfaces/Profile/ProfileInterface.cs
@@ -16,12 +16,17 @@
 namespace GadrocsWorkshop.Helios.Interfaces.Profile
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Linq;
     using GadrocsWorkshop.Helios.ComponentModel;
 
     [HeliosInterface("Helios.Base.ProfileInterface", "Profile", null, typeof(UniqueHeliosInterfaceFactory), AutoAdd = true)]
-    public class ProfileInterface : HeliosInterface
+    public class ProfileInterface : HeliosInterface, IStatusReportNotify, IResetMonitorsObserver
     {
+        // observers of our status report that need to be told when we change status
+        private readonly HashSet<IStatusReportObserver> _observers = new HashSet<IStatusReportObserver>();
+
         public ProfileInterface()
             : base("Profile")
         {
@@ -98,6 +103,63 @@ namespace GadrocsWorkshop.Helios.Interfaces.Profile
         public override void WriteXml(System.Xml.XmlWriter writer)
         {
             // No-Op
+        }
+
+        public void Subscribe(IStatusReportObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Unsubscribe(IStatusReportObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void InvalidateStatusReport()
+        {
+            if (_observers.Count < 1)
+            {
+                return;
+            }
+            IList<StatusReportItem> newReport = new List<StatusReportItem>();
+            if (Profile != null)
+            {
+                if (Profile.IsValidMonitorLayout)
+                {
+                    newReport.Add(new StatusReportItem
+                    {
+                        Status = "The monitor arrangement saved in this profile matches this computer",
+                        Flags = StatusReportItem.StatusFlags.ConfigurationUpToDate | StatusReportItem.StatusFlags.Verbose
+                    });
+                }
+                else
+                {
+                    newReport.Add(new StatusReportItem
+                    {
+                        Status = "The monitor arrangement saved in this profile does not match this computer",
+                        Recommendation = "Perform Reset Monitors from the Profile menu",
+                        Severity = StatusReportItem.SeverityCode.Error,
+                        Link = StatusReportItem.ProfileEditor,
+                        Flags = StatusReportItem.StatusFlags.ConfigurationUpToDate | StatusReportItem.StatusFlags.Verbose
+                    });
+
+                }
+            }
+            PublishStatusReport(newReport);
+        }
+
+        public void PublishStatusReport(IEnumerable<StatusReportItem> statusReport)
+        {
+            IList<StatusReportItem> statusReportItems = statusReport.ToList();
+            foreach (IStatusReportObserver observer in _observers)
+            {
+                observer.ReceiveStatusReport("Profile Interface", "Interface to Helios Control Center itself.", statusReportItems);
+            }
+        }
+
+        public void NotifyResetMonitorsComplete()
+        {
+            InvalidateStatusReport();
         }
     }
 }
