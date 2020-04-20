7acc68cdf72e9193964f144f6df06b4c0b9d58d7
derammo <817160+derammo@users.noreply.github.com>
2020-04-20 19:05:26 -0400

    implemented InitSubmodules tool

---
+++ Tools/InitSubmodules/InitSubmodules.cs
@@ -0,0 +1,29 @@
+﻿using LibGit2Sharp;
+using System;
+
+namespace InitSubmodules
+{
+    class InitSubmodules
+    {
+        static void Main(string[] args)
+        {
+            string repoRoot = Repository.Discover(Environment.CurrentDirectory) ??
+                throw new System.Exception("InitSubmodules must be run within a git repo.");
+            using (Repository repo = new Repository(repoRoot))
+            {
+                foreach (Submodule submodule in repo.Submodules)
+                {
+                    SubmoduleStatus submoduleStatus = submodule.RetrieveStatus();
+                    bool needsInit = submoduleStatus.HasFlag(SubmoduleStatus.WorkDirUninitialized);
+                    if (needsInit)
+                    {
+                        repo.Submodules.Update(submodule.Name, new SubmoduleUpdateOptions
+                        {
+                            Init = true
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
