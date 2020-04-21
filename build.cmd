1355525078bc131d59d8c47b649488b0e4001894
derammo <817160+derammo@users.noreply.github.com>
2020-04-20 22:19:00 -0400

    fixing command line build after build changes

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@
 @REM build with version stamps
 MSBuild.exe -binaryLogger:LogFile=clean.binlog -clp:WarningsOnly -warnAsMessage:MSB4078 -p:Configuration=Release;Platform=x64 -t:Clean Helios.sln
 @rmdir /s /q bin
-@MSBuild.exe -binaryLogger:LogFile=prebuild.binlog -clp:WarningsOnly -warnAsMessage:MSB4078 -p:version=%VERSION% -p:Configuration=Release;Platform=x64 Tools\GenerateSimulatorViewportTemplates\GenerateSimulatorViewportTemplates.csproj
+@MSBuild.exe -binaryLogger:LogFile=prebuild.binlog -clp:WarningsOnly -warnAsMessage:MSB4078 -p:version=%VERSION% -p:Configuration=Release;Platform=x64 BuildMeFirst.sln
 @MSBuild.exe -binaryLogger:LogFile=build.binlog  -clp:WarningsOnly -warnAsMessage:MSB4078 -p:version=%VERSION% -p:Configuration=NoInstallers;Platform=x64 Helios.sln
 @if %errorlevel% neq 0 (
 	echo build of "NoInstallers" configuration failed.  Installer will not be built.
@@ -11,7 +11,7 @@ MSBuild.exe -binaryLogger:LogFile=clean.binlog -clp:WarningsOnly -warnAsMessage:
 )
 
 @REM build installers; this requires https://stackoverflow.com/questions/8648428/an-error-occurred-while-validating-hresult-8000000a/45580775#45580775
-@devenv Helios.sln /Build JustInstallers Helios.sln
+@devenv Helios.sln /Build "JustInstallers|x64"
 
 @REM fix up installer
 @pushd "Helios Installer\Release"
