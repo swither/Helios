15d6734405f9286f313551767ca9b760e7ff3431
derammo <817160+derammo@users.noreply.github.com>
2020-04-21 23:11:34 -0400

    locations view improved

---
+++ Helios/Util/DCS/MockInstallationLocations.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections.Generic;
+
+namespace GadrocsWorkshop.Helios.Util.DCS
+{
+    class MockInstallationLocation
+    {
+        public string SavedGamesName { get; set; }
+        public string Path { get; set; }
+        public string Version { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+
+    class MockInstallationLocations
+    {
+        public IEnumerable<MockInstallationLocation> Items { get; } = new List<MockInstallationLocation>
+        {
+            new MockInstallationLocation
+            {
+                SavedGamesName = "FCS.OpenBeta",
+                Path = "C:\\Program Files\\Igloo Divonics\\FCS Wuerg\\",
+                Version = "3.4.5.23423",
+                IsEnabled = true
+            }
+        };
+    }
+}
