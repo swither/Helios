0a1b8551e6f847412300ae955c11d009272ad9f2
derammo <817160+derammo@users.noreply.github.com>
2020-04-29 17:57:31 -0400

    anonymizer support for URIs

--- Helios/Util/Anonymizer.cs
+++ Helios/Util/Anonymizer.cs
@@ -10,8 +10,12 @@ namespace GadrocsWorkshop.Helios.Util
         {
             // replace home directory reference
             {Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "%USERPROFILE%"},
+            // and file:// URIS
+            {Environment.GetFolderPath(Environment.SpecialFolder.UserProfile).Replace("\\", "/"), "%USERPROFILE%"},
             // just in case user name appears in other paths
-            {$"\\{Environment.UserName}\\", "\\%USERNAME%\\"}
+            {$"\\{Environment.UserName}\\", "\\%USERNAME%\\"},
+            // and URIs
+            {$"/{Environment.UserName}/", "/%USERNAME%/"}
         };
 
         public static string Anonymize(string value)
@@ -24,6 +28,12 @@ namespace GadrocsWorkshop.Helios.Util
             }
 
             return working;
+        }
+
+        public static string Anonymize(Uri uri)
+        {
+            // nothing special so far
+            return Anonymize((uri.ToString()));
         }
     }
 }
