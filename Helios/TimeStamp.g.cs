63c81aabd0b6474061b7029b3196684d8d682950
derammo <817160+derammo@users.noreply.github.com>
2020-05-10 12:59:00 -0400

    refreshed evaluation period to June 9

--- Helios/TimeStamp.g.cs
+++ Helios/TimeStamp.g.cs
@@ -12,7 +12,7 @@ namespace GadrocsWorkshop.Helios
         {
             get
             {
-                return new DateTime( 637235193260719142L, DateTimeKind.Utc);
+                return new DateTime( 637247266185027375L, DateTimeKind.Utc);
             }
         }
     }
