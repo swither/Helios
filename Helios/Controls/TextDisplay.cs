swither/Helios

commit ce4827f624df3bd03adb038222ac903271ab9976

TextDisplay now supports three auto scaling modes
none - doesn't auto scale
legacy - behaves like Helios 1.4 TextDisplay to avoid breaking layouts
height - accurately adjusts font size based on height

Helios/Controls/TextDisplay.cs
@@ -13,6 +13,9 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+using System;
+using System.Globalization;
+
 namespace GadrocsWorkshop.Helios.Controls
 {
     using GadrocsWorkshop.Helios.ComponentModel;
@@ -48,6 +51,8 @@ namespace GadrocsWorkshop.Helios.Controls
             _textFormat.HorizontalAlignment = TextHorizontalAlignment.Left;
             _textFormat.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(TextFormat_PropertyChanged);
             // _textFormat.FontFamily = FontManager.Instance.GetFontFamilyByName("SF Digital Readout");
+            _configuredFontSize = _textFormat.FontSize;
+            _referenceHeight = Height;
         }
 
         #region Properties
@@ -254,6 +259,12 @@ namespace GadrocsWorkshop.Helios.Controls
             }
             set
             {
+                _referenceHeight = Height;
+                if (value == _configuredFontSize)
+                {
+                    return;
+                }
+                _configuredFontSize = value;
                 double oldValue = _textFormat.FontSize;
                 _textFormat.FontSize = value;
                 OnPropertyChanged("FontSize", oldValue, value, true);
@@ -261,6 +272,36 @@ namespace GadrocsWorkshop.Helios.Controls
             }
         }
 
+        /// <summary>
+            /// backing field for property ScalingMode, contains
+            /// the selected automatic font size scaling mode
+            /// </summary>
+        private TextScalingMode _scalingMode;
+
+        /// <summary>
+        /// the font size we actually configured via the UI
+        /// </summary>
+        private double _configuredFontSize;
+
+        /// <summary>
+        /// the height this display had when the font size was configured
+        /// </summary>
+        private double _referenceHeight;
+
+        /// <summary>
+        /// the selected automatic font size scaling mode
+        /// </summary>
+        public TextScalingMode ScalingMode
+        {
+            get => _scalingMode;
+            set
+            {
+                if (_scalingMode == value) return;
+                TextScalingMode oldValue = _scalingMode;
+                _scalingMode = value;
+                OnPropertyChanged("ScalingMode", oldValue, value, true);
+            }
+        }
         #endregion
 
         void TextFormat_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
@@ -271,11 +312,30 @@ namespace GadrocsWorkshop.Helios.Controls
 
         protected override void PostUpdateRectangle(Rect previous, Rect current)
         {
-            if (previous.Height == 0)
-                return;
-            double scale = current.Height / previous.Height;
-            TextFormat.FontSize = Clamp(scale * TextFormat.FontSize, 1, 100);
-            // Logger.Warn("Font Size " + TextFormat.FontSize);
+            switch (ScalingMode)
+            {
+                case TextScalingMode.Height:
+                    if (_referenceHeight < 0.001)
+                    {
+                        TextFormat.FontSize = _configuredFontSize;
+                        break;
+                    }
+                    // avoid accumulating error from repeated resizing by calculating from a reference point
+                    TextFormat.FontSize = Clamp(_configuredFontSize * current.Height / _referenceHeight, 1, 300);
+                    break;
+                case TextScalingMode.None:
+                    return;
+                case TextScalingMode.Legacy:
+                    if (previous.Height != 0)
+                    {
+                        double scale = current.Height / previous.Height;
+                        TextFormat.FontSize = Clamp(scale * TextFormat.FontSize, 1, 100);
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            Logger.Debug("Font Size " + TextFormat.FontSize);
         }
 
         public override void Reset()
@@ -317,6 +377,10 @@ namespace GadrocsWorkshop.Helios.Controls
             writer.WriteElementString("ParserDictionary", ParserDictionary);
             writer.WriteElementString("UseBackground", boolConverter.ConvertToInvariantString(UseBackground));
             writer.WriteElementString("UseParserDictionary", boolConverter.ConvertToInvariantString(UseParseDictionary));
+            if (ScalingMode != TextScalingMode.Legacy)
+            {
+                writer.WriteElementString("ScalingMode", ScalingMode.ToString(CultureInfo.InvariantCulture));
+            }
             base.WriteXml(writer);
         }
 
@@ -339,10 +403,20 @@ namespace GadrocsWorkshop.Helios.Controls
             ParserDictionary = reader.ReadElementString("ParserDictionary");
             UseBackground = (bool)boolConverter.ConvertFromInvariantString(reader.ReadElementString("UseBackground"));
             UseParseDictionary = (bool)boolConverter.ConvertFromInvariantString(reader.ReadElementString("UseParserDictionary"));
+            if (reader.Name == "ScalingMode" && Enum.TryParse(reader.ReadElementString("ScalingMode"), out TextScalingMode configured))
+            {
+                ScalingMode = configured;
+            }
+            else
+            {
+                ScalingMode = TextScalingMode.Legacy;
+            }
             base.ReadXml(reader);
 
             // now the auto scaling has messed up our font size, so we restore it
             _textFormat.FontSize = fontSizeFromProfile;
+            _configuredFontSize = fontSizeFromProfile;
+            _referenceHeight = Height;
         }
 
         private double Clamp(double value, double min, double max)

Helios/Controls/TextDisplayAppearancePropertyEditor.xaml
@@ -5,12 +5,15 @@
              xmlns:d="http://schemas.microsoft.com/expression/blend/2008" 
              mc:Ignorable="d" 
              xmlns:Helios="clr-namespace:GadrocsWorkshop.Helios"
-             xmlns:HeliosSdk="clr-namespace:GadrocsWorkshop.Helios.Windows.Controls"                      
+             xmlns:HeliosSdk="clr-namespace:GadrocsWorkshop.Helios.Windows.Controls"
+             xmlns:controls="clr-namespace:GadrocsWorkshop.Helios.Controls"
              DataContext="{Binding RelativeSource={RelativeSource Self}}"
              Height="Auto" d:DesignWidth="180" d:DesignHeight="483.123">
     <HeliosSdk:HeliosPropertyEditor.Resources>
         <Helios:EnumConverter Type="{x:Type Helios:TextVerticalAlignment}" x:Key="VertAlignmentTypes" />
         <Helios:EnumConverter Type="{x:Type Helios:TextHorizontalAlignment}" x:Key="HorizAlignmentTypes" />
+        <Helios:EnumConverter Type="{x:Type controls:TextScalingMode}" x:Key="TextScalingModes" />
+
     </HeliosSdk:HeliosPropertyEditor.Resources>
     <Grid Background="Transparent" Margin="4">
         <Grid.ColumnDefinitions>
@@ -23,6 +26,7 @@
             <RowDefinition Height="Auto" />
             <RowDefinition Height="Auto" />
             <RowDefinition Height="Auto" />
+            <RowDefinition Height="Auto"/>
             <RowDefinition Height="Auto" />
             <RowDefinition Height="Auto" />
             <RowDefinition Height="Auto" />
@@ -54,50 +58,56 @@
             <Label.ToolTip>How the text will be aligned horizontally inside the TextDisplay.</Label.ToolTip>
         </Label>
         <ComboBox Grid.Column="1" Grid.Row="4" Margin="2" HorizontalAlignment="Left" ItemsSource="{Binding Source={StaticResource HorizAlignmentTypes}, Path=DisplayNames}" SelectedItem="{Binding Path=Control.TextFormat.HorizontalAlignment, Converter={StaticResource HorizAlignmentTypes}}" GotFocus="TurnTextDisplayOn" />
+        <Label Grid.Column="0" Grid.Row="5" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Top" Content="Auto Scaling">
+            <Label.ToolTip>How font size will be scaled if the control is scaled.</Label.ToolTip>
+        </Label>
+        <ComboBox Grid.Column="1" Grid.Row="5" Margin="2" HorizontalAlignment="Left" ItemsSource="{Binding Source={StaticResource TextScalingModes}, Path=DisplayNames}" SelectedItem="{Binding Path=Control.ScalingMode, Converter={StaticResource TextScalingModes}}"/>
 
-        <TextBlock Grid.Column="0" Grid.ColumnSpan="2" Grid.Row="5" Margin="4,10,2,2" Style="{StaticResource Heading2}">Text Padding</TextBlock>
-        <Label Grid.Column="0" Grid.Row="6" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Top" Content="Left">
+        <TextBlock Grid.Column="0" Grid.ColumnSpan="2" Grid.Row="6" Margin="4,10,2,2" Style="{StaticResource Heading2}">Text Padding</TextBlock>
+        <Label Grid.Column="0" Grid.Row="7" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Top" Content="Left">
             <Label.ToolTip>Amount of space on the left edge where text will not be rendered. Hold shift to move individually.</Label.ToolTip>
         </Label>
-        <Slider Grid.Column="1" Grid.Row="6" Minimum="0" Maximum="1" Value="{Binding Path=Control.TextFormat.PaddingLeft}" Margin="0,2" GotFocus="TurnTextDisplayOn" ValueChanged="LeftPaddingChanged" />
-        <Label Grid.Column="0" Grid.Row="7" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Top" Content="Right">
+        <Slider Grid.Column="1" Grid.Row="7" Minimum="0" Maximum="1" Value="{Binding Path=Control.TextFormat.PaddingLeft}" Margin="0,2" GotFocus="TurnTextDisplayOn" ValueChanged="LeftPaddingChanged" />
+        <Label Grid.Column="0" Grid.Row="8" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Top" Content="Right">
             <Label.ToolTip>Amount of space on the right edge where text will not be rendered. Hold shift to move individually.</Label.ToolTip>
         </Label>
-        <Slider Grid.Column="1" Grid.Row="7" Minimum="0" Maximum="1" Value="{Binding Path=Control.TextFormat.PaddingRight}" IsDirectionReversed="true" Margin="0,2" GotFocus="TurnTextDisplayOn"  ValueChanged="RightPaddingChanged" />
-        <Label Grid.Column="0" Grid.Row="8" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Top" Content="Top">
+        <Slider Grid.Column="1" Grid.Row="8" Minimum="0" Maximum="1" Value="{Binding Path=Control.TextFormat.PaddingRight}" IsDirectionReversed="true" Margin="0,2" GotFocus="TurnTextDisplayOn"  ValueChanged="RightPaddingChanged" />
+        <Label Grid.Column="0" Grid.Row="9" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Top" Content="Top">
             <Label.ToolTip>Amount of space on the top edge where text will not be rendered. Hold shift to move individually.</Label.ToolTip>
         </Label>
-        <Slider Grid.Column="1" Grid.Row="8" Minimum="0" Maximum="1" Value="{Binding Path=Control.TextFormat.PaddingTop}" Margin="0,2" GotFocus="TurnTextDisplayOn"  ValueChanged="TopPaddingChanged" />
-        <Label Grid.Column="0" Grid.Row="9" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Top" Content="Bottom">
+        <Slider Grid.Column="1" Grid.Row="9" Minimum="0" Maximum="1" Value="{Binding Path=Control.TextFormat.PaddingTop}" Margin="0,2" GotFocus="TurnTextDisplayOn"  ValueChanged="TopPaddingChanged" />
+        <Label Grid.Column="0" Grid.Row="10" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Top" Content="Bottom">
             <Label.ToolTip>Amount of space on the bottom edge where text will not be rendered. Hold shift to move individually.</Label.ToolTip>
         </Label>
-        <Slider Grid.Column="1" Grid.Row="9" Minimum="0" Maximum="1" Value="{Binding Path=Control.TextFormat.PaddingBottom}" IsDirectionReversed="true" Margin="0,2" GotFocus="TurnTextDisplayOn"  ValueChanged="BottomPaddingChanged" />
+        <Slider Grid.Column="1" Grid.Row="10" Minimum="0" Maximum="1" Value="{Binding Path=Control.TextFormat.PaddingBottom}" IsDirectionReversed="true" Margin="0,2" GotFocus="TurnTextDisplayOn"  ValueChanged="BottomPaddingChanged" />
 
-        <TextBlock Grid.Column="0" Grid.ColumnSpan="2" Grid.Row="10" Margin="4,10,2,2" Style="{StaticResource Heading2}">Colors</TextBlock>
-        <Label Grid.Column="0" Grid.Row="12" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Center" Content="Text">
+        <TextBlock Grid.Column="0" Grid.ColumnSpan="2" Grid.Row="11" Margin="4,10,2,2" Style="{StaticResource Heading2}">Colors</TextBlock>
+        <Label Grid.Column="0" Grid.Row="13" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Center" Content="Text">
             <Label.ToolTip>Color for the text.</Label.ToolTip>
         </Label>
-        <HeliosSdk:ColorWell Grid.Column="1" Grid.Row="12" Color="{Binding Path=Control.OnTextColor,Mode=TwoWay,UpdateSourceTrigger=PropertyChanged}" HorizontalAlignment="Left" Margin="0,2"  GotFocus="TurnTextDisplayOn" />
+        <HeliosSdk:ColorWell Grid.Column="1" Grid.Row="13"
+                             Color="{Binding Path=Control.OnTextColor,Mode=TwoWay,UpdateSourceTrigger=PropertyChanged}" HorizontalAlignment="Left" Margin="0,2"  GotFocus="TurnTextDisplayOn" />
 
-        <Label Grid.Column="0" Grid.Row="13" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Center" Content="Use Background">
+        <Label Grid.Column="0" Grid.Row="14" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Center" Content="Use Background">
             <Label.ToolTip>Color for the text.</Label.ToolTip>
         </Label>
-        <CheckBox Grid.Column="1" Grid.Row="13" VerticalAlignment="Center" HorizontalAlignment="Center" IsChecked="{Binding Control.UseBackground}" />
+        <CheckBox Grid.Column="1" Grid.Row="14" VerticalAlignment="Center" HorizontalAlignment="Center" IsChecked="{Binding Control.UseBackground}" />
 
-        <Label Grid.Column="0" Grid.Row="14" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Center" Content="BackGround">
+        <Label Grid.Column="0" Grid.Row="15" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Center" Content="BackGround">
             <Label.ToolTip>Color text will be rendered in when the TextDisplay is on.</Label.ToolTip>
         </Label>
 
-        <Label Grid.Column="0" Grid.Row="15" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Center" Content="Use Parser">
+        <Label Grid.Column="0" Grid.Row="16" FontSize="10" HorizontalAlignment="Right" VerticalAlignment="Center" Content="Use Parser">
             <Label.ToolTip>User Parsing Dictionary.</Label.ToolTip>
         </Label>
-        <CheckBox Grid.Column="1" Grid.Row="15" VerticalAlignment="Center" HorizontalAlignment="Center" IsChecked="{Binding Control.UseParseDictionary}" />
+        <CheckBox Grid.Column="1" Grid.Row="16" VerticalAlignment="Center" HorizontalAlignment="Center" IsChecked="{Binding Control.UseParseDictionary}" />
 
-        <HeliosSdk:ColorWell Grid.Column="1" Grid.Row="14" Color="{Binding Path=Control.BackgroundColor,Mode=TwoWay,UpdateSourceTrigger=PropertyChanged}" HorizontalAlignment="Left" Margin="0,2"  GotFocus="TurnTextDisplayOn" />
-        <Label Grid.Column="0" Grid.Row="16" FontSize="10" HorizontalAlignment="Right" Content="Parser Info">
+        <HeliosSdk:ColorWell Grid.Column="1" Grid.Row="15"
+                             Color="{Binding Path=Control.BackgroundColor,Mode=TwoWay,UpdateSourceTrigger=PropertyChanged}" HorizontalAlignment="Left" Margin="0,2"  GotFocus="TurnTextDisplayOn" />
+        <Label Grid.Column="0" Grid.Row="17" FontSize="10" HorizontalAlignment="Right" Content="Parser Info">
             <Label.ToolTip>List of replacements to be made in the strings received key1=val1;key2=val2....</Label.ToolTip>
         </Label>
-        <HeliosSdk:HeliosTextBox  Grid.Column="1" Grid.Row="16" FontSize="10" Text="{Binding Control.ParserDictionary}" Margin="2,2,2,-39" GotFocus="TurnTextDisplayOn"  TextChanged="HeliosTextParserBox_TextChanged"/>
+        <HeliosSdk:HeliosTextBox  Grid.Column="1" Grid.Row="17" FontSize="10" Text="{Binding Control.ParserDictionary}" Margin="2,2,2,-39" GotFocus="TurnTextDisplayOn"  TextChanged="HeliosTextParserBox_TextChanged"/>
 
     </Grid>
 </HeliosSdk:HeliosPropertyEditor>

Helios/Controls/TextScalingMode.cs
@@ -1,0 +1,30 @@
+﻿// Copyright 2020 Ammo Goettsch
+// 
+// Helios is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Helios is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System.ComponentModel;
+
+namespace GadrocsWorkshop.Helios.Controls
+{
+    public enum TextScalingMode
+    {
+        [Description("Adjust font size based on height of control")]
+        Height,
+        [Description("Do not automatically adjust the font size")]
+        None,
+        [Description("Use the legacy code from Helios 1.4")]
+        Legacy
+    }
+}

Helios/Helios.csproj
@@ -69,6 +69,7 @@
     <Reference Include="WindowsBase" />
   </ItemGroup>
   <ItemGroup>
+    <Compile Include="Controls\TextScalingMode.cs" />
     <Compile Include="Windows\Controls\About.xaml.cs">
       <DependentUpon>About.xaml</DependentUpon>
     </Compile>

commit 8c032a77126ac67699ec3602193fcc8de8b73a4b

fixed broken TextDisplay scaling implementation
introduced in ce4827f624df3bd03adb038222ac903271ab9976

Helios/Controls/TextDisplay.cs
@@ -49,9 +49,8 @@ namespace GadrocsWorkshop.Helios.Controls
         {
             _textFormat.VerticalAlignment = TextVerticalAlignment.Center;
             _textFormat.HorizontalAlignment = TextHorizontalAlignment.Left;
-            _textFormat.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(TextFormat_PropertyChanged);
+            _textFormat.PropertyChanged += TextFormat_PropertyChanged;
             // _textFormat.FontFamily = FontManager.Instance.GetFontFamilyByName("SF Digital Readout");
-            _configuredFontSize = _textFormat.FontSize;
             _referenceHeight = Height;
         }
 
@@ -251,37 +250,11 @@ namespace GadrocsWorkshop.Helios.Controls
             }
         }
 
-        public double FontSize
-        {
-            get
-            {
-                return _textFormat.FontSize;
-            }
-            set
-            {
-                _referenceHeight = Height;
-                if (value == _configuredFontSize)
-                {
-                    return;
-                }
-                _configuredFontSize = value;
-                double oldValue = _textFormat.FontSize;
-                _textFormat.FontSize = value;
-                OnPropertyChanged("FontSize", oldValue, value, true);
-                Refresh();
-            }
-        }
-
         /// <summary>
-            /// backing field for property ScalingMode, contains
-            /// the selected automatic font size scaling mode
-            /// </summary>
-        private TextScalingMode _scalingMode;
-
-        /// <summary>
-        /// the font size we actually configured via the UI
+        /// backing field for property ScalingMode, contains
+        /// the selected automatic font size scaling mode
         /// </summary>
-        private double _configuredFontSize;
+        private TextScalingMode _scalingMode;
 
         /// <summary>
         /// the height this display had when the font size was configured
@@ -306,10 +279,20 @@ namespace GadrocsWorkshop.Helios.Controls
 
         void TextFormat_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
+            if (e.PropertyName == nameof(TextFormat.ConfiguredFontSize))
+            {
+                // track these for recalculation of automatic scaling
+                _referenceHeight = Height;
+            }
+
+            // invalidate entire TextFormat
             OnPropertyChanged("TextFormat", (PropertyNotificationEventArgs)e);
+
+            // recalculate rendering
             OnDisplayUpdate();
         }
 
+        // WARNING: this virtual method is called from the base constructor (indirectly)
         protected override void PostUpdateRectangle(Rect previous, Rect current)
         {
             switch (ScalingMode)
@@ -317,11 +300,13 @@ namespace GadrocsWorkshop.Helios.Controls
                 case TextScalingMode.Height:
                     if (_referenceHeight < 0.001)
                     {
-                        TextFormat.FontSize = _configuredFontSize;
+                        TextFormat.FontSize = TextFormat.ConfiguredFontSize;
                         break;
                     }
                     // avoid accumulating error from repeated resizing by calculating from a reference point
-                    TextFormat.FontSize = Clamp(_configuredFontSize * current.Height / _referenceHeight, 1, 300);
+                    Logger.Debug("scaling font based on new height {Height} versus reference {ReferenceSize} at height {ReferenceHeight}",
+                        current.Height, TextFormat.ConfiguredFontSize, _referenceHeight);
+                    TextFormat.FontSize = Clamp(TextFormat.ConfiguredFontSize * current.Height / _referenceHeight, 1, 2000);
                     break;
                 case TextScalingMode.None:
                     return;
@@ -390,10 +375,6 @@ namespace GadrocsWorkshop.Helios.Controls
             reader.ReadStartElement("Font");
             _textFormat.ReadXml(reader);
 
-            // save this size, because the automatic scaling will keep increasing it when we read the size of our rectangle
-            // and we get called back on PostUpdateRectangle
-            double fontSizeFromProfile = _textFormat.FontSize;
-
             reader.ReadEndElement();
             OnTextColor = (Color)colorConverter.ConvertFromString(null, System.Globalization.CultureInfo.InvariantCulture, reader.ReadElementString("OnTextColor"));
             BackgroundColor = (Color)colorConverter.ConvertFromString(null, System.Globalization.CultureInfo.InvariantCulture, reader.ReadElementString("BackgroundColor"));
@@ -412,8 +393,7 @@ namespace GadrocsWorkshop.Helios.Controls
             base.ReadXml(reader);
 
             // now the auto scaling has messed up our font size, so we restore it
-            _textFormat.FontSize = fontSizeFromProfile;
-            _configuredFontSize = fontSizeFromProfile;
+            _textFormat.FontSize = _textFormat.ConfiguredFontSize;
             _referenceHeight = Height;
         }
 

Helios/TextFormat.cs
@@ -36,10 +36,16 @@ namespace GadrocsWorkshop.Helios
         private TextDecorations _decorations;
         private TextDecorationCollection _formattedDecorations;
 
-        private double _textPaddingLeft = 0d;
-        private double _textPaddingTop = 0d;
-        private double _textPaddingRight = 0d;
-        private double _textPaddingBottom = 0d;
+        private double _textPaddingLeft;
+        private double _textPaddingTop;
+        private double _textPaddingRight;
+        private double _textPaddingBottom;
+
+        /// <summary>
+        /// backing field for property ConfiguredFontSize, contains
+        /// the font size we actually configured via the UI
+        /// </summary>
+        private double _configuredFontSize = 12f;
 
         public TextFormat()
         {
@@ -123,6 +129,26 @@ namespace GadrocsWorkshop.Helios
                     double oldValue = _size;
                     _size = value;
                     OnPropertyChanged("FontSize", oldValue, value, true);
+                }
+            }
+        }
+
+        /// <summary>
+        /// the font size we actually configured via the UI
+        /// </summary>
+        public double ConfiguredFontSize
+        {
+            get
+            {
+                return _configuredFontSize;
+            }
+            set
+            {
+                if (!_configuredFontSize.Equals(value))
+                {
+                    double oldValue = _configuredFontSize;
+                    _configuredFontSize = value;
+                    OnPropertyChanged("ConfiguredFontSize", oldValue, value, true);
                 }
             }
         }
@@ -339,6 +365,7 @@ namespace GadrocsWorkshop.Helios
             _style = (FontStyle)fsc.ConvertFromString(null, System.Globalization.CultureInfo.InvariantCulture, reader.ReadElementString("FontStyle"));
             _weight = (FontWeight)fwc.ConvertFromString(null, System.Globalization.CultureInfo.InvariantCulture, reader.ReadElementString("FontWeight"));
             _size = double.Parse(reader.ReadElementString("FontSize"), CultureInfo.InvariantCulture);
+            _configuredFontSize = _size;
 
             if (reader.Name.Equals("HorizontalAlignment"))
             {

Helios/Windows/Controls/TextFormatButton.xaml.cs
@@ -85,6 +85,7 @@ namespace GadrocsWorkshop.Helios.Windows.Controls
                     TextFormat.FontStyle = dialog.SelectedTypeface.Style;
                     TextFormat.FontWeight = dialog.SelectedTypeface.Weight;
                     TextFormat.FontSize = dialog.SelectedSize;
+                    TextFormat.ConfiguredFontSize = dialog.SelectedSize;
 
                     TextDecorations newDecorations = 0;
                     if (dialog.IsUnderline)