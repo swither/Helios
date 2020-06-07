swither/Helios

commit c7e9d3bf0c3593e375595222d52990407626bf98

DCS Generic Interface now has specialized UI
internal: refactored DCS Interface to allow sharing of sections

Helios/Interfaces/DCS/Common/DCSInterface.cs
@@ -82,7 +82,7 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
             ExportFunctionsPath = exportFunctionsPath;
 
             // make sure we keep our list up to date and don't typo on the name of an export device
-            Debug.Assert(DCSVehicleImpersonation.KnownVehicles.Contains(exportDeviceName));
+            Debug.Assert(null == exportDeviceName || DCSVehicleImpersonation.KnownVehicles.Contains(exportDeviceName));
 
             // create handling for DCS export meta information we handle ourselves
             NetworkTriggerValue activeVehicle = new NetworkTriggerValue(this, "ACTIVE_VEHICLE", "ActiveVehicle",
@@ -132,7 +132,6 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
 
                 foreach (IStatusReportObserver observer in _observers)
                 {
-                    string newName = StatusName;
                     InvalidateStatusReport();
                 }
             }
@@ -408,6 +407,17 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
         {
             base.WriteXml(writer);
             TypeConverter enumConverter = TypeDescriptor.GetConverter(typeof(DCSExportModuleFormat));
+
+            // we could have configuration data that is not used and should not be persisted, so clean up if we can
+            if (Configuration != null)
+            {
+                DCSExportConfiguration.ModuleFormatInfo moduleInfo = Configuration.ExportModuleFormatInfo[ExportModuleFormat];
+                if (!moduleInfo.CanBeAttached)
+                {
+                    ExportModuleBaseName = null;
+                    ExportModuleText = null;
+                }
+            }
 
             if (ExportModuleFormat != DEFAULT_EXPORT_MODULE_FORMAT)
             {

Helios/Helios.csproj
@@ -69,6 +69,21 @@
   </ItemGroup>
   <ItemGroup>
     <Compile Include="Controls\TextScalingMode.cs" />
+    <Compile Include="Interfaces\DCS\Common\ExportConfigurationSection.xaml.cs">
+      <DependentUpon>ExportConfigurationSection.xaml</DependentUpon>
+    </Compile>
+    <Compile Include="Interfaces\DCS\Common\ExportFilesOutputSection.xaml.cs">
+      <DependentUpon>ExportFilesOutputSection.xaml</DependentUpon>
+    </Compile>
+    <Compile Include="Interfaces\DCS\Common\ExportLuaOutputSection.xaml.cs">
+      <DependentUpon>ExportLuaOutputSection.xaml</DependentUpon>
+    </Compile>
+    <Compile Include="Interfaces\DCS\Common\PhantomMonitorFixSection.xaml.cs">
+      <DependentUpon>PhantomMonitorFixSection.xaml</DependentUpon>
+    </Compile>
+    <Compile Include="Interfaces\DCS\Generic\GenericInterfaceEditor.xaml.cs">
+      <DependentUpon>GenericInterfaceEditor.xaml</DependentUpon>
+    </Compile>
     <Compile Include="Windows\Controls\About.xaml.cs">
       <DependentUpon>About.xaml</DependentUpon>
     </Compile>
@@ -128,6 +143,26 @@
     <Page Include="Interfaces\ControlRouter\ControlRouterEditor.xaml">
       <SubType>Designer</SubType>
       <Generator>MSBuild:Compile</Generator>
+    </Page>
+    <Page Include="Interfaces\DCS\Common\ExportConfigurationSection.xaml">
+      <Generator>MSBuild:Compile</Generator>
+      <SubType>Designer</SubType>
+    </Page>
+    <Page Include="Interfaces\DCS\Common\ExportFilesOutputSection.xaml">
+      <Generator>MSBuild:Compile</Generator>
+      <SubType>Designer</SubType>
+    </Page>
+    <Page Include="Interfaces\DCS\Common\ExportLuaOutputSection.xaml">
+      <Generator>MSBuild:Compile</Generator>
+      <SubType>Designer</SubType>
+    </Page>
+    <Page Include="Interfaces\DCS\Common\PhantomMonitorFixSection.xaml">
+      <SubType>Designer</SubType>
+      <Generator>MSBuild:Compile</Generator>
+    </Page>
+    <Page Include="Interfaces\DCS\Generic\GenericInterfaceEditor.xaml">
+      <Generator>MSBuild:Compile</Generator>
+      <SubType>Designer</SubType>
     </Page>
     <Page Include="Interfaces\Unsupported\UnsupportedInterfaceEditor.xaml">
       <SubType>Designer</SubType>

Helios/Interfaces/DCS/Common/DCSExportConfiguration.cs
@@ -60,9 +60,10 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
             public string DisplayName { get; internal set; }
             public string ModuleLocation { get; internal set; }
             public bool CanBeAttached { get; internal set; }
+            public bool CanGenerate { get; internal set; }
         }
 
-        public static readonly Dictionary<DCSExportModuleFormat, ModuleFormatInfo> ExportModuleFormatInfo =
+        public readonly Dictionary<DCSExportModuleFormat, ModuleFormatInfo> ExportModuleFormatInfo =
             new Dictionary<DCSExportModuleFormat, ModuleFormatInfo>
             {
                 {
@@ -71,7 +72,8 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
                         // capitalization for use in a sentence (not at the beginning)
                         DisplayName = "Helios driver generated by this interface",
                         ModuleLocation = "Drivers",
-                        CanBeAttached = false
+                        CanBeAttached = false,
+                        CanGenerate = true
                     }
                 },
                 {
@@ -80,7 +82,8 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
                         // capitalization for use in a sentence (not at the beginning)
                         DisplayName = "export module provided by Capt Zeen (Version 1)",
                         ModuleLocation = "Mods",
-                        CanBeAttached = true
+                        CanBeAttached = true,
+                        CanGenerate = false
                     }
                 }
             };
@@ -428,7 +431,6 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
 
         private void UpdateConfiguration()
         {
-
             // reconfigure everything
             UpdateDirectories();
             UpdateExportStub();
@@ -636,6 +638,7 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
                 };
             }
 
+            ModuleFormatInfo moduleInfo = ExportModuleFormatInfo[_parent.ExportModuleFormat];
             foreach (InstallationLocation location in installationLocations)
             {
                 // check on the Export.lua stub, if applicable
@@ -653,7 +656,8 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
                 UpdateExportScript();
                 yield return CheckExportScript(location);
 
-                if (_parent.ExportModuleFormat != DCSExportModuleFormat.HeliosDriver16 && string.IsNullOrEmpty(_parent.ExportModuleBaseName))
+
+                if (string.IsNullOrEmpty(_parent.ExportModuleBaseName) && !moduleInfo.CanGenerate)
                 {
                     yield return new StatusReportItem
                     {
@@ -705,18 +709,30 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
 
         private void UpdateModule()
         {
+            ModuleFormatInfo moduleInfo = ExportModuleFormatInfo[_parent.ExportModuleFormat];
+            if (moduleInfo.CanBeAttached && _parent.ExportModuleBaseName != null)
+            {
+                // nothing to do
+                return;
+            }
+
+            if (!moduleInfo.CanGenerate)
+            {
+                // not allowed
+                return;
+            }
+
+            // otherwise, try to generate the module text
             switch (_parent.ExportModuleFormat)
             {
                 case DCSExportModuleFormat.HeliosDriver16:
-                    // NOTE: if this is refactored, continue to ensure that nothing is 
-                    // written to _exportDriver if any component throws
                     _exportModuleText =
                         string.Join("\n", GenerateDriverLines())
                         + FetchExportFunctions()
                         + "\nreturn driver";
                     break;
                 case DCSExportModuleFormat.CaptZeenModule1:
-                    // nothing to do
+                    // don't know how
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
@@ -934,25 +950,29 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
         // subtly different in terms of severity and we want to encourage more special cases added in the future
         private StatusReportItem CheckModule(InstallationLocation location)
         {
-            ModuleFormatInfo moduleInfo = ExportModuleFormatInfo[_parent.ExportModuleFormat];
-            switch (_parent.ExportModuleFormat)
+            if ((_parent.ImpersonatedVehicleName ?? _parent.VehicleName) == null)
             {
-                case DCSExportModuleFormat.HeliosDriver16:
-                    break;
-                case DCSExportModuleFormat.CaptZeenModule1:
-                    if (_parent.ExportModuleBaseName == null)
-                    {
-                        // no included module and we don't even know its name
-                        return new StatusReportItem
-                        {
-                            Status = $"The {moduleInfo.DisplayName} is not included in this profile, so Helios cannot check if it is installed.",
-                            Recommendation = "Please manually check that the export module was correctly placed",
-                            Flags = StatusReportItem.StatusFlags.ConfigurationUpToDate
-                        };
-                    }
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                // this will happen when Generic or similar interface is not configured completely
+                return new StatusReportItem
+                {
+                    Status = $"Vehicle name must be configured for interfaces that can be used for different vehicles.",
+                    Recommendation = $"Configure vehicle name for {_parent.Name}",
+                    Severity = StatusReportItem.SeverityCode.Error,
+                    Flags = StatusReportItem.StatusFlags.ConfigurationUpToDate,
+                    Link = StatusReportItem.ProfileEditor
+                };
+            }
+
+            ModuleFormatInfo moduleInfo = ExportModuleFormatInfo[_parent.ExportModuleFormat];
+            if (_parent.ExportModuleBaseName == null && !moduleInfo.CanGenerate)
+            {
+                // no included module and we don't even know its name
+                return new StatusReportItem
+                {
+                    Status = $"The {moduleInfo.DisplayName} is not included in this profile, so Helios cannot check if it is installed.",
+                    Recommendation = "Please manually check that the export module was correctly placed",
+                    Flags = StatusReportItem.StatusFlags.ConfigurationUpToDate
+                };
             }
 
             string exportModulePath = location.ExportModulePath(moduleInfo.ModuleLocation, GenerateModuleBaseName());

Helios/Interfaces/DCS/Common/ExportConfigurationSection.xaml
@@ -1,0 +1,37 @@
+<GroupBox x:Class="GadrocsWorkshop.Helios.Interfaces.DCS.Common.ExportConfigurationSection"
+	xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+	xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" 
+    xmlns:d="http://schemas.microsoft.com/expression/blend/2008" 
+    mc:Ignorable="d"
+    xmlns:common="clr-namespace:GadrocsWorkshop.Helios.Interfaces.DCS.Common"
+    d:DataContext="{d:DesignInstance Type=common:DCSInterfaceEditor}"
+    Header="DCS Export Configuration" 
+    Style="{StaticResource BasicGroup}">
+    <Grid>
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition Width="Auto" MinWidth="120"/>
+            <ColumnDefinition Width="*"/>
+        </Grid.ColumnDefinitions>
+        <Grid.RowDefinitions>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+        </Grid.RowDefinitions>
+        <Label Grid.Column="0" Grid.Row="0" HorizontalAlignment="Right" Content="IP Address"/>
+        <TextBox Grid.Column="1" Grid.Row="0" VerticalAlignment="Center" HorizontalAlignment="Left" Width="200"
+                 MaxWidth="200" Text="{Binding Path=Configuration.IPAddress}"/>
+        <TextBlock Grid.Column="1" Grid.Row="1" Style="{StaticResource Documentation}" Text="IP Address to which Export script will send data.  If you are running on the same machine 127.0.0.1 is recommended."/>
+        <Label Grid.Column="0" Grid.Row="2" HorizontalAlignment="Right" Content="Port"/>
+        <TextBox Grid.Column="1" Grid.Row="2" VerticalAlignment="Center" HorizontalAlignment="Left" Width="40"
+                 MaxWidth="40" Text="{Binding Path=Configuration.Port}"/>
+        <TextBlock Grid.Column="1" Grid.Row="3" Style="{StaticResource Documentation}" Text="UDP port number to which Export script will send data and on which Helios will listen."/>
+        <Label Grid.Column="0" Grid.Row="4" HorizontalAlignment="Right" Content="Export Frequency"/>
+        <TextBox Grid.Column="1" Grid.Row="4" VerticalAlignment="Center" HorizontalAlignment="Left" Width="40"
+                 MaxWidth="40" Text="{Binding Path=Configuration.ExportFrequency}"/>
+        <TextBlock Grid.Column="1" Grid.Row="5" Style="{StaticResource Documentation}" Text="Number of times per second that instrument gauges will be exported (minimum of 4)."/>
+    </Grid>
+</GroupBox>

Helios/Interfaces/DCS/Common/ExportConfigurationSection.xaml.cs
@@ -1,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
+{
+    public partial class ExportConfigurationSection: GroupBox
+    {
+        public ExportConfigurationSection()
+        {
+            InitializeComponent();
+        }
+    }
+}

Helios/Interfaces/DCS/Common/ExportFilesOutputSection.xaml
@@ -1,0 +1,92 @@
+<GroupBox x:Class="GadrocsWorkshop.Helios.Interfaces.DCS.Common.ExportFilesOutputSection"
+	xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+	xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+	xmlns:util="clr-namespace:GadrocsWorkshop.Helios.Util"
+    xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" 
+    xmlns:d="http://schemas.microsoft.com/expression/blend/2008" 
+    mc:Ignorable="d"
+    xmlns:common="clr-namespace:GadrocsWorkshop.Helios.Interfaces.DCS.Common"
+    d:DataContext="{d:DesignInstance Type=common:DCSInterfaceEditor}"
+    Header="DCS Export Files Output" 
+    Style="{StaticResource BasicGroup}">
+    <GroupBox.Resources>
+        <Style x:Key="UpToDateStyle" TargetType="{x:Type Panel}">
+            <Setter Property="UIElement.Visibility" Value="Collapsed"/>
+            <Style.Triggers>
+                <MultiDataTrigger>
+                    <MultiDataTrigger.Conditions>
+                        <Condition Binding="{Binding Path=Configuration.Status}" Value="{x:Static util:StatusCodes.UpToDate}"/>
+                    </MultiDataTrigger.Conditions>
+                    <MultiDataTrigger.Setters>
+                        <Setter Property="UIElement.Visibility" Value="Visible"/>
+                    </MultiDataTrigger.Setters>
+                </MultiDataTrigger>
+            </Style.Triggers>
+        </Style>
+        <Style x:Key="OutOfDateStyle" TargetType="{x:Type Panel}">
+            <Setter Property="UIElement.Visibility" Value="Collapsed"/>
+            <Style.Triggers>
+                <MultiDataTrigger>
+                    <MultiDataTrigger.Conditions>
+                        <Condition Binding="{Binding Path=Configuration.Status}" Value="{x:Static util:StatusCodes.OutOfDate}"/>
+                    </MultiDataTrigger.Conditions>
+                    <MultiDataTrigger.Setters>
+                        <Setter Property="UIElement.Visibility" Value="Visible"/>
+                    </MultiDataTrigger.Setters>
+                </MultiDataTrigger>
+            </Style.Triggers>
+        </Style>
+        <Style x:Key="NoLocationsStyle" TargetType="{x:Type Panel}">
+            <Setter Property="UIElement.Visibility" Value="Hidden"/>
+            <Style.Triggers>
+                <MultiDataTrigger>
+                    <MultiDataTrigger.Conditions>
+                        <Condition Binding="{Binding Path=Configuration.Status}" Value="{x:Static util:StatusCodes.NoLocations}"/>
+                    </MultiDataTrigger.Conditions>
+                    <MultiDataTrigger.Setters>
+                        <Setter Property="UIElement.Visibility" Value="Visible"/>
+                    </MultiDataTrigger.Setters>
+                </MultiDataTrigger>
+            </Style.Triggers>
+        </Style>
+    </GroupBox.Resources>
+    <Grid Height="Auto">
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition Width="Auto" MinWidth="120"/>
+            <!-- primary labels -->
+            <ColumnDefinition Width="*"/>
+            <!-- primary controls and long messages-->
+        </Grid.ColumnDefinitions>
+        <Grid.RowDefinitions>
+            <RowDefinition Height="10"/>
+            <RowDefinition Height="Auto"/>
+        </Grid.RowDefinitions>
+
+        <Label Grid.Row="1" Grid.Column="0" Style="{StaticResource InterfaceEditorLabel}" Content="Status"/>
+        <Grid Grid.Row="1" Grid.Column="1" Style="{StaticResource UpToDateStyle}">
+            <Grid.ColumnDefinitions>
+                <ColumnDefinition Width="*"/>
+                <ColumnDefinition Width="Auto"/>
+            </Grid.ColumnDefinitions>
+            <TextBlock Grid.Row="0" Grid.Column="0" Style="{StaticResource InterfaceEditorStatus}">Your DCS configuration is up to date.</TextBlock>
+        </Grid>
+        <Grid Grid.Row="1" Grid.Column="1" Style="{StaticResource NoLocationsStyle}">
+            <Grid.ColumnDefinitions>
+                <ColumnDefinition Width="*"/>
+                <ColumnDefinition Width="Auto"/>
+            </Grid.ColumnDefinitions>
+            <TextBlock Grid.Row="0" Grid.Column="0" Style="{StaticResource InterfaceEditorStatus}" Foreground="Red">You must configure DCS installation locations before you can configure this interface.</TextBlock>
+        </Grid>
+        <Grid Grid.Row="1" Grid.Column="1" Style="{StaticResource OutOfDateStyle}">
+            <Grid.ColumnDefinitions>
+                <ColumnDefinition Width="*"/>
+                <ColumnDefinition Width="Auto"/>
+            </Grid.ColumnDefinitions>
+            <Grid.RowDefinitions>
+                <RowDefinition Height="Auto"/>
+            </Grid.RowDefinitions>
+            <TextBlock Grid.Row="0" Grid.Column="0" Style="{StaticResource InterfaceEditorStatus}" Foreground="Red">An updated export script and associated files must be installed to support this profile in DCS.</TextBlock>
+            <Button Grid.Row="0" Grid.Column="1" Command="{Binding ConfigureCommand}" Style="{StaticResource InterfaceEditorButton}">Setup DCS</Button>
+        </Grid>
+    </Grid>
+</GroupBox>

Helios/Interfaces/DCS/Common/ExportFilesOutputSection.xaml.cs
@@ -1,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
+{
+    public partial class ExportFilesOutputSection: GroupBox
+    {
+        public ExportFilesOutputSection()
+        {
+            InitializeComponent();
+        }
+    }
+}

Helios/Interfaces/DCS/Common/ExportLuaOutputSection.xaml
@@ -1,0 +1,73 @@
+<Expander x:Class="GadrocsWorkshop.Helios.Interfaces.DCS.Common.ExportLuaOutputSection"
+	xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+	xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" 
+    xmlns:d="http://schemas.microsoft.com/expression/blend/2008" 
+    mc:Ignorable="d"
+    xmlns:common="clr-namespace:GadrocsWorkshop.Helios.Interfaces.DCS.Common"
+    d:DataContext="{d:DesignInstance Type=common:DCSInterfaceEditor}"
+    Header="DCS Export.Lua Output (Advanced)" 
+    Margin="0,0,0,12" 
+    IsExpanded="False" 
+    Style="{StaticResource GroupBoxWithExpanderStyle}">
+    <Expander.Resources>
+        <Style x:Key="AppliesIfGenerateExportLoader" TargetType="{x:Type Control}">
+            <Style.Triggers>
+                <DataTrigger Binding="{Binding Path=Configuration.GenerateExportLoader}" Value="False">
+                    <Setter Property="IsEnabled" Value="false"/>
+                    <Setter Property="Foreground" Value="Gray"/>
+                </DataTrigger>
+            </Style.Triggers>
+        </Style>
+        <DataTemplate x:Key="DoFileTemplate">
+            <StackPanel Orientation="Horizontal" Margin="6,3">
+                <Button Template="{StaticResource DeleteButton}" Command="common:DCSInterfaceEditor.RemoveDoFileCommand"
+                        CommandParameter="{Binding}"/>
+                <TextBlock Margin="6,0,0,0" Text="{Binding}"/>
+            </StackPanel>
+        </DataTemplate>
+    </Expander.Resources>
+    <Grid Height="Auto">
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition Width="Auto" MinWidth="120"/>
+            <!-- primary labels -->
+            <ColumnDefinition Width="*"/>
+            <!-- primary controls and long messages-->
+        </Grid.ColumnDefinitions>
+        <Grid.RowDefinitions>
+            <RowDefinition Height="10"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="10"/>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="10"/>
+        </Grid.RowDefinitions>
+
+        <Label Grid.Row="1" Grid.Column="0" VerticalAlignment="Center" Content="Export.lua"/>
+        <StackPanel Grid.Row="1" Grid.Column="1" Orientation="Horizontal">
+            <!-- shielded from stretching -->
+            <CheckBox VerticalAlignment="Center" IsChecked="{Binding Path=Configuration.GenerateExportLoader}"/>
+        </StackPanel>
+        <TextBlock Grid.Row="2" Grid.Column="1" Style="{StaticResource Documentation}">Uncheck to generate only the export files in the Helios subdirectory, to use your own Export.lua stub that you already have.</TextBlock>
+
+        <GroupBox Grid.Row="4" Grid.Column="1" Header="Third Party Scripts" Margin="0,4,0,0" Style="{StaticResource AppliesIfGenerateExportLoader}">
+            <StackPanel>
+                <TextBlock Margin="4,4,4,12" TextWrapping="Wrap"><Run Text="Helios will add the following scripts as dofile lines when it writes Export.lua. Enter paths relative to the directory containing Export.lua."/><LineBreak/><Run Text="For example: ./TARS/TARSExport.lua"/></TextBlock>
+                <Border BorderBrush="#FF808080" BorderThickness="0,1" Margin="12">
+                    <ItemsControl ItemTemplate="{StaticResource DoFileTemplate}" ItemsSource="{Binding Path=Configuration.DoFiles}"/>
+                </Border>
+                <Grid>
+                    <Grid.ColumnDefinitions>
+                        <ColumnDefinition Width="*"/>
+                        <ColumnDefinition Width="Auto"/>
+                    </Grid.ColumnDefinitions>
+                    <Grid.RowDefinitions>
+                        <RowDefinition Height="Auto"/>
+                    </Grid.RowDefinitions>
+                    <TextBox Grid.Row="0" Grid.Column="0" VerticalAlignment="Center" Margin="0,0,12,0" TextWrapping="Wrap" Name="NewDoFile"/>
+                    <Button Grid.Row="0" Grid.Column="1" Margin="0,0,7,0" Width="120" Height="24" Command="common:DCSInterfaceEditor.AddDoFileCommand" CommandParameter="{Binding ElementName=NewDoFile}">Add File</Button>
+                </Grid>
+            </StackPanel>
+        </GroupBox>
+    </Grid>
+</Expander>

commit 4b7c566f61283f625c1c2773487e6eeb23231fdd

temporary patch around bug introduced in c7e9d3bf

Helios/Interfaces/DCS/Common/DCSInterface.cs
@@ -20,6 +20,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Windows;
 using System.Xml;
 using GadrocsWorkshop.Helios.Interfaces.Capabilities;
 using GadrocsWorkshop.Helios.Interfaces.Capabilities.ProfileAwareInterface;
@@ -409,15 +410,8 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
             TypeConverter enumConverter = TypeDescriptor.GetConverter(typeof(DCSExportModuleFormat));
 
             // we could have configuration data that is not used and should not be persisted, so clean up if we can
-            if (Configuration != null)
-            {
-                DCSExportConfiguration.ModuleFormatInfo moduleInfo = Configuration.ExportModuleFormatInfo[ExportModuleFormat];
-                if (!moduleInfo.CanBeAttached)
-                {
-                    ExportModuleBaseName = null;
-                    ExportModuleText = null;
-                }
-            }
+            // XXX remove this dispatch once we eliminate the profile writing thread
+            Application.Current.Dispatcher.Invoke(CleanBeforeWriting);
 
             if (ExportModuleFormat != DEFAULT_EXPORT_MODULE_FORMAT)
             {
@@ -453,6 +447,19 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
             if (ImpersonatedVehicleName != null)
             {
                 writer.WriteElementString("ImpersonatedVehicleName", ImpersonatedVehicleName);
+            }
+        }
+
+        private void CleanBeforeWriting()
+        {
+            if (Configuration != null)
+            {
+                DCSExportConfiguration.ModuleFormatInfo moduleInfo = Configuration.ExportModuleFormatInfo[ExportModuleFormat];
+                if (!moduleInfo.CanBeAttached)
+                {
+                    ExportModuleBaseName = null;
+                    ExportModuleText = null;
+                }
             }
         }
 