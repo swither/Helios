8968475e6df2ebf4f69555e0e0132b232dbf2526
derammo <817160+derammo@users.noreply.github.com>
2020-07-07 23:41:43 -0400

    TODO comment removed pending more discussion whether this is real

--- Helios/Interfaces/DCS/Common/DCSExportConfiguration.cs
+++ Helios/Interfaces/DCS/Common/DCSExportConfiguration.cs
@@ -702,8 +702,7 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
         {
             _exportMain = Resources
                 .ReadResourceFile($"pack://application:,,,/Helios;component/Interfaces/DCS/Common/{EXPORT_MAIN_NAME}")
-                // TODO: Currently the IP address is set in the inidividual airtcraft interface, but implemented with a wider scope. 
-                // TODO: support DNS lookup for hostnames (currently the IPV4 address is not policed)
+                // TODO: validate the IP address against allowable protocol versions and address types for this version of HeliosExport__.lua
                 .Replace("HELIOS_REPLACE_IPAddress", IPAddress)
                 .Replace("HELIOS_REPLACE_Port", Port.ToString())
                 .Replace("HELIOS_REPLACE_ExportInterval", Math.Round(1d / Math.Max(4, ExportFrequency), 3).ToString(CultureInfo.InvariantCulture));
