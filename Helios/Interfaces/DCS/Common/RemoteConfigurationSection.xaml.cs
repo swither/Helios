e26de9b09cc7cac224fde997bc92fe7dcc70bca7
derammo <817160+derammo@users.noreply.github.com>
2020-08-16 07:09:52 -0400

    added port number configuration to DCS Interface for remote Helios

---
+++ Helios/Interfaces/DCS/Common/RemoteConfigurationSection.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace GadrocsWorkshop.Helios.Interfaces.DCS.Common
+{
+    public partial class RemoteConfigurationSection : GroupBox
+    {
+        public RemoteConfigurationSection()
+        {
+            InitializeComponent();
+        }
+    }
+}
