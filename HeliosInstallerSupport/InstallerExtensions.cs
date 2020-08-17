726b9224bb615b8fb0c8cf084a4b2ee7fd6f86da
derammo <817160+derammo@users.noreply.github.com>
2020-08-16 20:56:29 -0400

    notes and todo items created on installer helper

--- HeliosInstallerSupport/InstallerExtensions.cs
+++ HeliosInstallerSupport/InstallerExtensions.cs
@@ -17,6 +17,14 @@ namespace HeliosInstallerSupport
             }
         }
 
+        // XXX for safety, we should also handle post install, so that if the helper fails to run we roll back
+        // otherwise, we can get in a situation where we can install but not uninstall
+        // WARNING: don't run the minimal Helios on install or we will create folders
+
+        // REVISIT: make this helper conditional on a public property that can be set from command line to turn it off for such recovery cases
+        // NOTE: command line can apparently only set properties that already exist in the table, and the setup project does not let us
+        // create one so we have to do it in the VBS
+
         protected override void OnBeforeUninstall(IDictionary savedState)
         {
             if (!MinimalHelios.CanStart(out string failureMessage))
