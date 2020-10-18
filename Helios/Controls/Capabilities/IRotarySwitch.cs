fe93f6eea94433bfb19a5f7cba5a0d7cc568d5b0
derammo <817160+derammo@users.noreply.github.com>
2020-10-17 21:59:39 -0400

    additonal control router support for rotary switches
    control router can now optionally deliver pulses directly
    to rotary switches so that input devices with detents can
    be mapped to rotary switch detents

---
+++ Helios/Controls/Capabilities/IRotarySwitch.cs
@@ -0,0 +1,26 @@
+﻿namespace GadrocsWorkshop.Helios.Controls.Capabilities
+{
+    public interface IRotarySwitch: IPulsedControl
+    {
+        /// <summary>
+        /// the current position, in the range MinPosition..MaxPosition (inclusive)
+        /// and which can be changed by adding or subtracting via Pulse(...)
+        /// </summary>
+        int CurrentPosition { get; }
+
+        /// <summary>
+        /// minimum position number of the switch
+        /// </summary>
+        int MinPosition { get; }
+
+        /// <summary>
+        /// maximum position number of the switch
+        /// </summary>
+        int MaxPosition { get; }
+
+        /// <summary>
+        /// true if this switch wraps around
+        /// </summary>
+        bool IsContinuous { get; }
+    }
+}
