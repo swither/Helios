869a4554a357827c6e27382e9c8126b2b09a17c0
derammo <817160+derammo@users.noreply.github.com>
2020-10-25 19:39:03 -0400

    implemented reset composite control background

--- Helios/Controls/BackgroundImageAppearanceEditor.xaml.cs
+++ Helios/Controls/BackgroundImageAppearanceEditor.xaml.cs
@@ -1,4 +1,5 @@
 ﻿//  Copyright 2014 Craig Courtney
+//  Copyright 2020 Ammo Goettsch
 //    
 //  Helios is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -13,7 +14,9 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+using System.Windows.Input;
 using GadrocsWorkshop.Helios.Controls.Capabilities;
+using GadrocsWorkshop.Helios.Windows;
 
 namespace GadrocsWorkshop.Helios.Controls
 {
@@ -29,6 +32,40 @@ namespace GadrocsWorkshop.Helios.Controls
         public BackgroundImageEditor()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// backing field for property ResetCommand, contains
+        /// command handlers for resetting the background image path
+        /// </summary>
+        private ICommand _resetCommand;
+
+        /// <summary>
+        /// command handlers for resetting the background image path
+        /// </summary>
+        public ICommand ResetCommand
+        {
+            get
+            {
+            _resetCommand = _resetCommand ?? new RelayCommand(
+                parameter =>
+                {
+                    if (!(Control is IConfigurableBackgroundImage configurable))
+                    {
+                        return;
+                    }
+                    configurable.BackgroundImage = configurable.DefaultBackgroundImage;
+                },
+                parameter =>
+                {
+                    if (!(Control is IConfigurableBackgroundImage configurable))
+                    {
+                        return false;
+                    }
+                    return configurable.BackgroundImage != configurable.DefaultBackgroundImage;
+                });
+                return _resetCommand;
+            }
         }
     }
 }
