0b209cc83a929824a647450a78dac3e06e0cca6f
linknetx <forums@linknetmail.com>
2021-06-26 16:35:02 +0100

    Update Altimeter.cs

--- HeliosFalcon/Gauges/Altimeter.cs
+++ HeliosFalcon/Gauges/Altimeter.cs
@@ -69,15 +69,9 @@ namespace GadrocsWorkshop.Helios.Gauges.F_16.AltimeterBMS
 
         void Altitude_Execute(object action, HeliosActionEventArgs e)
         {
-            double _altValue = (e.Value.DoubleValue < 0 ? 99999 - Math.Abs(e.Value.DoubleValue) : e.Value.DoubleValue);
-            double _tensDrumRotationEnd = Math.Ceiling(_altValue / 1000d) * 1000d;
-            double _tensDrumRotationStart = _tensDrumRotationEnd - 1000d;
-
             _needle.Rotation = _needleCalibration.Interpolate(e.Value.DoubleValue % 1000d);
-            _tensDrum.Value = _altValue > _tensDrumRotationStart
-                ? Math.Floor(_altValue / 1000d) + (_altValue - _tensDrumRotationStart) / (_tensDrumRotationEnd - _tensDrumRotationStart)
-                : Math.Floor(_altValue / 1000d);
-            _drum.Value = _altValue;
+            _tensDrum.Value = (e.Value.DoubleValue < 0 ? 99999 - Math.Abs(e.Value.DoubleValue) : e.Value.DoubleValue)/1000d;
+            _drum.Value = e.Value.DoubleValue < 0 ? 99999 - Math.Abs(e.Value.DoubleValue) : e.Value.DoubleValue;
         }
 
         void AirPressure_Execute(object action, HeliosActionEventArgs e)
