0f79bad1dba2c681703c43dd0134babba635e9b9
wheelchock <6605805+Todd1215@users.noreply.github.com>
2021-07-15 15:11:33 -0400

    adding back

--- HeliosFalcon/Interfaces/BMS/BMSFalconDataExporter.cs
+++ HeliosFalcon/Interfaces/BMS/BMSFalconDataExporter.cs
@@ -485,6 +485,7 @@ namespace GadrocsWorkshop.Helios.Interfaces.Falcon.BMS
                 _stringAreaSize = _lastFlightData2.StringAreaSize;
                 _stringAreaTime = _lastFlightData2.StringAreaTime;
 
+                SetValue("Altimeter", "indicated altitude", new BindingValue(-_lastFlightData2.aauz));
                 SetValue("Altimeter", "barimetric pressure", new BindingValue(_lastFlightData2.AltCalReading));
 
                 SetValue("HSI", "nav mode", new BindingValue((int)_lastFlightData2.navMode));
