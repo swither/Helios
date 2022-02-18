a18319b9c1b3715208112feda82a6f0563b9f2bb
linknetx <forums@linknetmail.com>
2022-02-18 23:00:42 +0000

    Falcon Interface - EHSI Gauge Reset Fix

--- HeliosFalcon/Gauges/EHSI/EHSI.cs
+++ HeliosFalcon/Gauges/EHSI/EHSI.cs
@@ -14,7 +14,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace GadrocsWorkshop.Helios.Controls
+namespace GadrocsWorkshop.Helios.Gauges.Falcon.EHSI
 {
 	using GadrocsWorkshop.Helios.ComponentModel;
 	using GadrocsWorkshop.Helios.Interfaces.Falcon;
@@ -337,6 +337,8 @@ namespace GadrocsWorkshop.Helios.Controls
 			_TextData.TacanChannel = 0;
 			_TextData.TacanBand = 0;
 			_TextData.TacanMode = 0;
+
+			ProcessDataValues();
 		}
 
 		#endregion Methods
