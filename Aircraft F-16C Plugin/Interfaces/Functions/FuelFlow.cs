f1833cca6d4328227776a91582a6698e33492741
BlueFinBima <BlueFinBima@users.noreply.github.com>
2024-04-07 11:02:21 +0100

    F-16C project creation and assets moved into new dll (with re-directions). Abandonment of separate F-15E installation package and removal of the build code to support that (as well as the GitHub actions). Added F-16C Block 50 Fuel gauge. Adding in untested DCS F-16C interface which will be hidden

---
+++ Aircraft F-16C Plugin/Interfaces/Functions/FuelFlow.cs
@@ -0,0 +1,117 @@
+﻿//  Copyright 2014 Craig Courtney
+//  Copyright 2020 Ammo Goettsch
+//    
+//  Helios is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Helios is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace GadrocsWorkshop.Helios.Interfaces.DCS.F16C.Functions
+{
+    using GadrocsWorkshop.Helios.Interfaces.DCS.Common;
+    using GadrocsWorkshop.Helios.UDPInterface;
+    using GadrocsWorkshop.Helios.Util;
+    using System;
+    using System.Globalization;
+
+    public class FuelFlow : DCSFunction
+    {
+        private static ExportDataElement[] DataElementsTemplate;
+
+        private HeliosValue _fuel;
+
+        public FuelFlow(BaseUDPInterface sourceInterface) : this(sourceInterface, "2091", "Fuel System", "Fuel Flow", "Fuel Flow rate.") { }    
+        public FuelFlow(BaseUDPInterface sourceInterface, string arg, string device, string elementName, string description)
+            : base(sourceInterface, device, elementName, description)
+        {
+            DataElementsTemplate[0] = new DCSDataElement(arg, null, true);
+            DoBuild();
+        }
+
+        // deserialization constructor
+        public FuelFlow(BaseUDPInterface sourceInterface, System.Runtime.Serialization.StreamingContext context)
+            : base(sourceInterface, context)
+        {
+            // no code
+        }
+
+        public override void BuildAfterDeserialization()
+        {
+            DoBuild();
+        }
+
+        private void DoBuild()
+        {
+            _fuel = new HeliosValue(SourceInterface, BindingValue.Empty, SerializedDeviceName, SerializedFunctionName,
+                SerializedDescription, "", BindingValueUnits.PoundsPerHour);
+            Values.Add(_fuel);
+            Triggers.Add(_fuel);
+        }
+
+        protected override ExportDataElement[] DefaultDataElements => DataElementsTemplate;
+
+        public override void ProcessNetworkData(string id, string value)
+        {
+            string[] parts = Tokenizer.TokenizeAtLeast(value, 3, ';');
+
+            double tenThousands = ClampedParse(parts[0], 10000d);
+            double thousands = ClampedParse(parts[1], 1000d);
+            double hundreds = Parse(parts[2], 100d);
+
+            double fuel = tenThousands + thousands + hundreds;
+            _fuel.SetValue(new BindingValue(fuel), false);
+        }
+
+        private double Parse(string value, double scale)
+        {
+            if (!double.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture.NumberFormat,
+                out double scaledValue))
+            {
+                return scaledValue;
+            }
+
+            if (scaledValue < 1.0d)
+            {
+                scaledValue *= scale * 10d;
+            }
+            else
+            {
+                scaledValue = 0d;
+            }
+            return scaledValue;
+        }
+
+        private double ClampedParse(string value, double scale)
+        {
+            if (!double.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture.NumberFormat,
+                out double scaledValue))
+            {
+                return scaledValue;
+            }
+
+            if (scaledValue < 1.0d)
+            {
+                scaledValue = Math.Truncate(scaledValue * 10d) * scale;
+            }
+            else
+            {
+                scaledValue = 0d;
+            }
+            return scaledValue;
+        }
+
+
+        public override void Reset()
+        {
+            _fuel.SetValue(BindingValue.Empty, true);
+        }
+    }
+}
